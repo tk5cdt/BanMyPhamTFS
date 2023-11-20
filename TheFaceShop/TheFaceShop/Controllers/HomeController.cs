bfba88547d073ff0ad55fea8e1be834db36bbd90
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2023-11-20 21:09:57 +0700

    add package folder

--- TheFaceShop/TheFaceShop/Controllers/HomeController.cs
+++ TheFaceShop/TheFaceShop/Controllers/HomeController.cs
@@ -13,5 +13,10 @@ namespace TheFaceShop.Controllers
         {
             return View();
         }
+
+        public ActionResult DonHang()
+        {
+            return View();
+        }
     }
 }
