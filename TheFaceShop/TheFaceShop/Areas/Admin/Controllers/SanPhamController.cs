0c60013050321231d8bde8f33e68fa300065400f
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2023-12-03 18:10:39 +0700

    add ckeditor

---
+++ TheFaceShop/TheFaceShop/Areas/Admin/Controllers/SanPhamController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheFaceShop.Models;
+
+namespace TheFaceShop.Areas.Admin.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        QL_THEFACESHOPEntities db = new QL_THEFACESHOPEntities();
+        public ActionResult ShowProduct()
+        {
+            return View(db.SANPHAMs.ToList());
+        }
+        // GET: Admin/SanPham
+        public ActionResult CreateProduct()
+        {
+            return View();
+        }
+    }
+}
