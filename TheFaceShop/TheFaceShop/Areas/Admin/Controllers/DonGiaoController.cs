4dc9bf860192353d44e59499cb3b7a151a576d60
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2023-12-05 20:41:35 +0700

    add view dongiao

---
+++ TheFaceShop/TheFaceShop/Areas/Admin/Controllers/DonGiaoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheFaceShop.Models;
+
+namespace TheFaceShop.Areas.Admin.Controllers
+{
+    public class DonGiaoController : Controller
+    {
+
+        // GET: Admin/DonGiao
+        QL_THEFACESHOPEntities db = new QL_THEFACESHOPEntities();
+        [Authorize(Roles = "QuanTri")]
+        public ActionResult DanhSachDonGiao()
+        {
+            return View(db.DONGIAOs);
+        }
+    }
+}
