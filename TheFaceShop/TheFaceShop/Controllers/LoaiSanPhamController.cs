b29970980f15beef1b945690c95ceabac9ad5c2b
WinWantWan <143304302+WinWantWan@users.noreply.github.com>
2023-12-06 03:21:02 +0700

    Add Home,  Danh muc, Layoutchung

---
+++ TheFaceShop/TheFaceShop/Controllers/LoaiSanPhamController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheFaceShop.Models;
+
+namespace TheFaceShop.Controllers
+{
+    public class LoaiSanPhamController : Controller
+    {
+        private QL_THEFACESHOPEntities db = new QL_THEFACESHOPEntities();
+
+        public ActionResult DanhMuc(string categoryId, string subCategoryId, string sortOrder)
+        {
+            var categories = db.LOAIs.ToList();
+
+            if (!string.IsNullOrEmpty(categoryId))
+            {
+                var category = db.LOAIs.FirstOrDefault(ct => ct.MALOAI == categoryId);
+                var subCategories = db.CHITIETLOAIs.Where(ct => ct.MALOAI == categoryId).ToList();
+
+                ViewBag.Category = category;
+                ViewBag.SubCategories = subCategories;
+
+                if (!string.IsNullOrEmpty(subCategoryId))
+                {
+                    var subCategory = db.CHITIETLOAIs.FirstOrDefault(ct => ct.MACTLOAI == subCategoryId);
+                    var products = db.SANPHAMs.Where(sp => sp.MACTL_SP == subCategoryId).ToList();
+
+                    ViewBag.SubCategory = subCategory;
+
+                    switch (sortOrder)
+                    {
+                        case "Giá tăng dần":
+                            products = products.OrderBy(p => p.GIABAN).ToList();
+                            break;
+                        case "Giá giảm dần":
+                            products = products.OrderByDescending(p => p.GIABAN).ToList();
+                            break;
+                        default:
+                            break;
+                    }
+
+                    ViewBag.Products = products;
+                }
+            }
+            return View(categories);
+        }
+    }
+}
