4696683ee4713bdb8d19d2aaf8ab497a3d0b5668
WinWantWan <143304302+WinWantWan@users.noreply.github.com>
2023-12-06 19:45:46 +0700

    Update danhmuc sảnphẩm

--- TheFaceShop/TheFaceShop/Controllers/PartialController.cs
+++ TheFaceShop/TheFaceShop/Controllers/PartialController.cs
@@ -21,5 +21,47 @@ namespace TheFaceShop.Controllers
             var sanPhams = db.SANPHAMs.Where(sp => sp.CHITIETLOAISP.CHITIETLOAI.MACTLOAI == "CTL001");
             return View(sanPhams);
         }
+        
+        public ActionResult ShowSanPham()
+        {
+            var sanPhams = db.SANPHAMs.ToList();
+            return View(sanPhams);
+        }
+
+        public ActionResult FilterByPrice(List<string> prices)
+        {
+            // Xử lí yêu cầu lọc sản phẩm dựa trên giá
+            // Ở đây, bạn có thể phân tích các giá trị và lọc sản phẩm dựa trên khoảng giá
+            // Ví dụ:
+            var filteredProducts = new List<SANPHAM>();
+
+            foreach (var priceRange in prices)
+            {
+                switch (priceRange)
+                {
+                    case "Dưới 500.000đ":
+                        // Lọc các sản phẩm dưới 500,000 VND
+                        filteredProducts.AddRange(db.SANPHAMs.Where(sp => sp.GIABAN < 500000));
+                        break;
+                    case "500.000đ - 1.000.000đ":
+                        // Lọc các sản phẩm từ 500,000 đến 1,000,000 VND
+                        filteredProducts.AddRange(db.SANPHAMs.Where(sp => sp.GIABAN >= 500000 && sp.GIABAN <= 1000000));
+                        break;
+                        // Thêm các trường hợp khác tương tự nếu cần
+                }
+            }
+
+            // Loại bỏ các sản phẩm trùng lặp
+            var uniqueFilteredProducts = filteredProducts.Distinct().ToList();
+
+            return PartialView("ShowSanPham", uniqueFilteredProducts);
+        }
+
+        public ActionResult FilterBySubcategory(string subcategoryId)
+        {
+            // Lọc sản phẩm dựa trên loại sản phẩm con
+            var filteredProducts = db.SANPHAMs.Where(p => p.MACTL_SP == subcategoryId).ToList();
+            return PartialView("ShowSanPham", filteredProducts);
+        }
     }
 }
