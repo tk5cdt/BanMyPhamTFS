841ef0f093e9ffe910f18320e0836a62b3a21623
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2023-12-13 18:08:39 +0700

    add BinhLuan

--- TheFaceShop/TheFaceShop/Controllers/SPController.cs
+++ TheFaceShop/TheFaceShop/Controllers/SPController.cs
@@ -28,13 +28,35 @@ namespace TheFaceShop.Controllers
                 return HttpNotFound();
             }
         }
-        protected override void HandleUnknownAction(string actionName)
+        [HttpPost]
+        public ActionResult BinhLuan(string masp, string bl = "", int mddg = 0)
         {
-            if (HttpContext.Response.StatusCode == 401 &&
-                HttpContext.Session["user"] == null)
+            var sp = db.SANPHAMs.SingleOrDefault(n => n.MASP == masp);
+            ViewBag.SPLQ = db.SANPHAMs.Where(n => n.MACTL_SP == sp.MACTL_SP);
+            var kh = Session["user"] as KHACHHANG;
+            var dg = db.DONGIAOs.Where(n => n.CTDGs.Any(m => m.MASP == masp) && n.KHACHHANG.MAKH == kh.MAKH);
+
+            if (dg.Count() > 0)
             {
-                RedirectToAction("DangNhap", "Account");
+                foreach (var item in dg)
+                {
+                    var ctdg = item.CTDGs.FirstOrDefault(n => n.MASP == masp);
+                    if (ctdg.BINHLUAN == null && ctdg.MUCDODG == null)
+                    {
+                        ctdg.BINHLUAN = bl;
+                        ctdg.MUCDODG = mddg;
+                        db.SaveChanges();
+                        ViewBag.TB = "Đánh giá thành công!";
+                        return View("ChiTietSanPham", sp);
+                    }
+                }
+                ViewBag.TB = "Bạn đã đánh giá sản phẩm này rồi!";
             }
+            else
+            {
+                ViewBag.TB = "Bạn cần phải mua sản phẩm để đánh giá";
+            }
+            return View("ChiTietSanPham", sp);
         }
     }
 }
